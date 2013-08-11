aa3aba3aba419d5b8874572fb0d407c75a17de1c
Paul Bakker <paul.bakker.nl@gmail.com>
2013-08-11 23:52:47 +0200

    Using requirejs in frontend

--- webshop.frontend.ui/static/js/webshop.app.js
+++ webshop.frontend.ui/static/js/webshop.app.js
@@ -1,93 +1,13 @@
-var webshopModule = angular.module('webshop', ['components', 'webshop.filters']).
-  config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-  	  when('/products', {templateUrl: 'partials/products.html',   controller: ProductsCtrl}).
-      when('/products/:category', {templateUrl: 'partials/products.html',   controller: ProductsCtrl}).
-      when('/basket', {templateUrl: 'partials/basket.html',   controller: BasketCtrl}).
-      when('/checkout', {templateUrl: 'partials/checkout.html',   controller: CheckoutCtrl}).
-      when('/ordercompleted', {templateUrl: 'partials/ordercompleted.html'}).
-      when('/orders', {templateUrl: 'partials/orders.html', controller: OrdersCtrl}).
-      otherwise({redirectTo: '/products'});
-}]);
+define(['angular', 'webshop.controllers', 'webshop.filters', 'webshop.services', 'webshop.directives'], function(angular) {
 
-webshopModule.factory('basket', function() {
-  var products = [];
-  if( localStorage['basket'] != undefined) {
-    products = JSON.parse(localStorage['basket']);
-  } else {
-    localStorage['basket'] = '[]';
-  }
-
-  return {
-    products: products,
-    addProduct: function(product) {
-      var updated = false;
-
-      angular.forEach(products, function(productInBasket) {
-        if(product._id == productInBasket._id) {
-          product.amount++;          
-          updated = true;
-        }
-      });
-
-      if(!updated) {
-        product.amount = 1;
-        products.push(product);
-      }
-
-      localStorage['basket'] = JSON.stringify(products);
-    },
-
-    removeItem: function(product) {
-      var idxToRemove = -1;
-      angular.forEach(products, function(productInBasket, idx) {
-        if(product._id == productInBasket._id) {
-          if(product.amount > 1) {
-            product.amount--;
-          } else {
-            idxToRemove = idx;
-          }          
-        }
-      });
-
-      if(idxToRemove != -1) {
-        products.splice(idxToRemove, 1);
-      }
-
-      localStorage['basket'] = JSON.stringify(products);
-    },
-
-    size: function() {
-      var total = 0;
-      angular.forEach(products, function(product) {
-        total += product.amount;
-      });
-
-      return total;
-    },
-
-    total: function() {
-      var total = 0;
-      angular.forEach(products, function(product) {
-        total += (product.amount * product.price);
-      });
-
-      return total;
-    },
-
-    empty: function() {
-      products = [];
-      localStorage['basket'] = JSON.stringify(products);
-    }
-  };
+  angular.module('webshop.app', ['webshop.controllers', 'webshop.services', 'webshop.directives', 'webshop.filters']).config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+    	when('/products', {templateUrl: 'partials/products.html',   controller: 'ProductsCtrl'}).
+        when('/products/:category', {templateUrl: 'partials/products.html',   controller: 'ProductsCtrl'}).
+        when('/basket', {templateUrl: 'partials/basket.html',   controller: 'BasketCtrl'}).
+        when('/checkout', {templateUrl: 'partials/checkout.html',   controller: 'CheckoutCtrl'}).
+        when('/ordercompleted', {templateUrl: 'partials/ordercompleted.html'}).
+        when('/orders', {templateUrl: 'partials/orders.html', controller: 'OrdersCtrl'}).
+        otherwise({redirectTo: '/products'});
+  }]);
 });
-
-angular.module('components', [])
-	.directive('navbar', function() {
-		return {
-			restrict: 'E',
-			controller: 'CategoriesCtrl',
-			templateUrl: 'partials/navbar.html'
-		}
-	}
-);
